f1ec691e59f20ec6086023018bc53bcbb56630c4
Terasel <133141424+Terasel@users.noreply.github.com>
2024-06-17 13:25:06 +0200

    Add files via upload

--- Entrega 2.1/debo.test.ts
+++ Entrega 2.1/debo.test.ts
@@ -2,20 +2,16 @@ import { debounce } from './debo';
 import { describe, expect, beforeEach, afterEach, jest, test } from '@jest/globals';
 
 
-describe('debounce module', () => {
+describe('mòdul debounce', () => {
   beforeEach(() => {
     jest.useFakeTimers();
   });
   afterEach(() => {
     jest.useRealTimers();
   });
-  test("funcionamiento correcto de la función debounce", () => {
+  test("funcionamient correcte de la funció debounce", () => {
     const callback = jest.fn();
     const debounced = debounce(callback, 500);
-    debounced();
-    expect(callback).not.toBeCalled();
-
-    jest.advanceTimersByTime(100);
     debounced();
     expect(callback).not.toBeCalled();
 
@@ -24,6 +20,13 @@ describe('debounce module', () => {
 
     jest.advanceTimersByTime(1);
     expect(callback).toBeCalledTimes(1);
+
+    jest.advanceTimersByTime(1);
+    expect(callback).toBeCalledTimes(1);
+
+    debounced();
+    jest.advanceTimersByTime(500);
+    expect(callback).toBeCalledTimes(2);
   });
 })
 
